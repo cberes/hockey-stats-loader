3f507bc9b46ef7a2e197b011381fcca901c04826
Corey Beres <corey.beres@gmail.com>
2016-08-17 12:08:14 -0400

    replace function with bigger queries

--- src/main/sql/hockey_stats.sql
+++ src/main/sql/hockey_stats.sql
@@ -95,53 +95,71 @@ INSERT INTO public.stat (name, description) VALUES
 ('Fenwick%Behind(2)', 'Fenwick for % - behind 2'),
 ('Fenwick%Behind(3)', 'Fenwick for % - behind 3+');
 
-CREATE OR REPLACE FUNCTION
-    adjust_for_score(in TEXT, in BIGINT, in INT, out value DOUBLE PRECISION)
-    AS $$ SELECT (( 3.75 * (COALESCE(s_a2.value, 0.0) - 0.440) +
-              8.46 * (COALESCE(s_a1.value, 0.0) - 0.461) +
-             17.94 * (COALESCE(s_ev.value, 0.0) - 0.500) +
-              8.46 * (COALESCE(s_b1.value, 0.0) - 0.539) +
-              3.75 * (COALESCE(s_b2.value, 0.0) - 0.560)
-            ) / 42.36
-           ) + 0.5
-    FROM stat ev --ON ev.name = (CAST($1 AS TEXT) || 'Even')
-    LEFT OUTER JOIN game_stat s_ev on s_ev.stat_id = ev._id
-      AND s_ev.game_id = $2 AND s_ev.team_id = $3
-    JOIN stat a1 ON a1.name = (CAST($1 AS TEXT) || 'Ahead(1)')
-    LEFT OUTER JOIN game_stat s_a1 on s_a1.stat_id = ev._id
-      AND s_a1.game_id = $2 AND s_a1.team_id = $3
-    JOIN stat a2 ON a2.name = (CAST($1 AS TEXT) || 'Ahead(2)')
-    LEFT OUTER JOIN game_stat s_a2 on s_a2.stat_id = ev._id
-      AND s_a2.game_id = $2 AND s_a2.team_id = $3
-    JOIN stat b1 ON b1.name = (CAST($1 AS TEXT) || 'Behind(1)')
-    LEFT OUTER JOIN game_stat s_b1 on s_b1.stat_id = ev._id
-      AND s_b1.game_id = $2 AND s_b1.team_id = $3
-    JOIN stat b2 ON b2.name = (CAST($1 AS TEXT) || 'Behind(2)')
-    LEFT OUTER JOIN game_stat s_b2 on s_b2.stat_id = ev._id
-      AND s_b2.game_id = $2 AND s_b2.team_id = $3
-    WHERE ev.name = (CAST($1 AS TEXT) || 'Even')
-    $$ LANGUAGE SQL STABLE;
+-- SA Corsi% by game and team
+SELECT g._id, t._id,
+(( 3.75 * (COALESCE(s_a2.value, 0.0) - 0.440) +
+   8.46 * (COALESCE(s_a1.value, 0.0) - 0.461) +
+  17.94 * (COALESCE(s_ev.value, 0.0) - 0.500) +
+   8.46 * (COALESCE(s_b1.value, 0.0) - 0.539) +
+   3.75 * (COALESCE(s_b2.value, 0.0) - 0.560)
+  ) / 42.36
+) + 0.5
+FROM game g
+JOIN team t ON g.home_team_id = t._id OR g.away_team_id = t._id
+JOIN stat ev ON ev.name = 'Corsi%Even'
+JOIN stat a1 ON a1.name = 'Corsi%Ahead(1)'
+JOIN stat a2 ON a2.name = 'Corsi%Ahead(2)'
+JOIN stat b1 ON b1.name = 'Corsi%Behind(1)'
+JOIN stat b2 ON b2.name = 'Corsi%Behind(2)'
+LEFT OUTER JOIN game_stat s_ev ON s_ev.stat_id = ev._id AND s_ev.game_id = g._id AND s_ev.team_id = t._id
+LEFT OUTER JOIN game_stat s_a1 ON s_a1.stat_id = a1._id AND s_a1.game_id = g._id AND s_a1.team_id = t._id
+LEFT OUTER JOIN game_stat s_a2 ON s_a2.stat_id = a2._id AND s_a2.game_id = g._id AND s_a2.team_id = t._id
+LEFT OUTER JOIN game_stat s_b1 ON s_b1.stat_id = b1._id AND s_b1.game_id = g._id AND s_b1.team_id = t._id
+LEFT OUTER JOIN game_stat s_b2 ON s_b2.stat_id = b2._id AND s_b2.game_id = g._id AND s_b2.team_id = t._id
+group by g._id, t._id, s_ev.value, s_a1.value, s_a2.value, s_b1.value, s_b2.value
 
-CREATE OR REPLACE FUNCTION
-    avg_stat(in TEXT, in INT, in TIMESTAMP, in TIMESTAMP, out value DOUBLE PRECISION)
-    AS $$ SELECT avg(adjust_for_score($1, g._id, $2))
-    FROM game g
-    JOIN game_result r ON r.game_id = g._id
-    WHERE (g.home_team_id = $2 OR g.away_team_id = $2) AND g.scheduled >= $3 AND g.scheduled < $4
-    $$ LANGUAGE SQL STABLE;
-
-CREATE OR REPLACE VIEW predictions AS
-    SELECT g._id as game_id
-          ,CAST(5.3 * avg_stat('Corsi%', g.home_team_id, g.scheduled - interval '2 months', g.scheduled) AS INT) AS home_score
-          ,CAST(5.3 * avg_stat('Corsi%', g.away_team_id, g.scheduled - interval '2 months', g.scheduled) AS INT) AS away_score
-    FROM game g;
+-- Past SA Corsi% by game and team
+CREATE OR REPLACE VIEW past_corsi_rel AS
+SELECT gg._id as game_id, t._id as team_id, avg(
+(( 3.75 * (COALESCE(s_a2.value, 0.0) - 0.440) +
+   8.46 * (COALESCE(s_a1.value, 0.0) - 0.461) +
+  17.94 * (COALESCE(s_ev.value, 0.0) - 0.500) +
+   8.46 * (COALESCE(s_b1.value, 0.0) - 0.539) +
+   3.75 * (COALESCE(s_b2.value, 0.0) - 0.560)
+  ) / 42.36
+) + 0.5) as corsi_rel
+FROM game gg
+JOIN team t ON gg.home_team_id = t._id OR gg.away_team_id = t._id
+JOIN stat ev ON ev.name = 'Corsi%Even'
+JOIN stat a1 ON a1.name = 'Corsi%Ahead(1)'
+JOIN stat a2 ON a2.name = 'Corsi%Ahead(2)'
+JOIN stat b1 ON b1.name = 'Corsi%Behind(1)'
+JOIN stat b2 ON b2.name = 'Corsi%Behind(2)'
+JOIN game g ON (g.home_team_id = t._id OR g.away_team_id = t._id)
+    AND g.scheduled >= gg.scheduled - interval '2 months' AND g.scheduled < gg.scheduled
+JOIN game_result r ON r.game_id = g._id
+LEFT OUTER JOIN game_stat s_ev ON s_ev.stat_id = ev._id AND s_ev.game_id = g._id AND s_ev.team_id = t._id
+LEFT OUTER JOIN game_stat s_a1 ON s_a1.stat_id = a1._id AND s_a1.game_id = g._id AND s_a1.team_id = t._id
+LEFT OUTER JOIN game_stat s_a2 ON s_a2.stat_id = a2._id AND s_a2.game_id = g._id AND s_a2.team_id = t._id
+LEFT OUTER JOIN game_stat s_b1 ON s_b1.stat_id = b1._id AND s_b1.game_id = g._id AND s_b1.team_id = t._id
+LEFT OUTER JOIN game_stat s_b2 ON s_b2.stat_id = b2._id AND s_b2.game_id = g._id AND s_b2.team_id = t._id
+group by gg._id, t._id;
 
 -- training data
-SELECT avg_stat('Corsi%', g.home_team_id, g.scheduled - interval '2 months', g.scheduled) as home_stat
-      ,avg_stat('Corsi%', g.away_team_id, g.scheduled - interval '2 months', g.scheduled) as away_stat
-      ,r.home_score
-      ,r.away_score
+SELECT 
+ r.home_score as home_score
+,d1.corsi_rel as home_corsi_rel
+,r.away_score as away_score
+,d2.corsi_rel as away_corsi_rel
 FROM game g
-JOIN game_result r ON g._id = r.game_id
+JOIN game_result r ON r.game_id = g._id
+JOIN past_corsi_rel d1 ON g._id = d1.game_id AND g.home_team_id = d1.team_id
+JOIN past_corsi_rel d2 ON g._id = d2.game_id AND g.away_team_id = d2.team_id
 LIMIT 10000;
+
+CREATE OR REPLACE VIEW predictions AS
+SELECT g._id as game_id
+      ,CAST(5.3 * avg_stat('Corsi%', g.home_team_id, g.scheduled - interval '2 months', g.scheduled) AS INT) AS home_score
+      ,CAST(5.3 * avg_stat('Corsi%', g.away_team_id, g.scheduled - interval '2 months', g.scheduled) AS INT) AS away_score
+FROM game g;
 
